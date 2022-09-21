39b9cff8c664a142ee2683652aff81b97b7d1efa
amrlsymlx <amirulsyamil.ahmad@gmail.com>
2022-09-21 20:38:35 +0800

    commit

---
+++ src/components/message.js
@@ -0,0 +1,29 @@
+
+import React, { useState } from 'react';
+import ConsumeCatalogAPI from './api';
+import MessageSize from './messagesize';
+
+function Message(props) {
+
+    const [likes, updateLikes]  =  useState(0)
+    
+    const increaseLikes=()=>{
+        updateLikes(likes +1)
+    }
+ 
+    return ( 
+        <div>
+            <p>
+                Message {props.children}: {props.msg} 
+                <br />
+                <button onClick={increaseLikes}>Like</button> {likes}
+                <br />
+                <MessageSize msg={props.msg}></MessageSize>
+
+            </p>
+            
+        </div>
+     );
+}
+
+export default Message;
